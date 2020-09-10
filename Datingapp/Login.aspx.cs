6df2b5ed4702f23ce2a1911b97a6df4229088140
Guneet Thind <41204772+guneethind@users.noreply.github.com>
2020-09-10 12:58:47 +0530

    correcting amrit upload files directory

--- Datingapp/Login.aspx.cs
+++ Datingapp/Login.aspx.cs
@@ -13,10 +13,5 @@ namespace Datingapp
         {
 
         }
-
-        protected void Button1_Click(object sender, EventArgs e)
-        {
-            Response.Redirect("Chat.aspx");
-        }
     }
 }
--- Datingapp/Login.aspx.designer.cs
+++ Datingapp/Login.aspx.designer.cs
@@ -15,6 +15,42 @@ namespace Datingapp
     {
 
         /// <summary>
+        /// Label1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
+        /// TextBox1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+
+        /// <summary>
+        /// Label2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+
+        /// <summary>
+        /// TextBox2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox2;
+
+        /// <summary>
         /// Button1 control.
         /// </summary>
         /// <remarks>
