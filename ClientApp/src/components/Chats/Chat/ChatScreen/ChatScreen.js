76abe4df3f6046aec91b42cabc99150bc1bf873e
Ashutosh <ashutosh94g@gmail.com>
2020-11-02 23:10:09 +0530

    chat screen bug fixed

--- ClientApp/src/components/Chats/Chat/ChatScreen/ChatScreen.js
+++ ClientApp/src/components/Chats/Chat/ChatScreen/ChatScreen.js
@@ -21,39 +21,62 @@ class ChatScreen extends Component {
 			}).catch(error => {
 				console.log(error)
 			})
+	};
+
+	componentDidUpdate() {
+		axios.defaults.headers.common['Authorization'] = "Bearer " + this.props.token;
+		axios.get(`api/users/${this.props.bio.id}/messages/thread/${this.props.user}`)
+			.then(response => {
+				if (response.data !== this.state.messages)
+					{
+						this.setState({messages: response.data})
+					}
+			}).catch(error => {
+				console.log(error)
+			})
 	}
 
 	changeHandler = (e) => {
 		this.setState({ input: e.target.value })
 	};
 
-	submitHandler = () => {
+	submitHandler = (e) => {
+		e.preventDefault();
 		console.log(this.state.input);
-	}
+		axios.post(`api/users/${this.props.bio.id}/messages`,
+			{ senderId: this.props.bio.id, receiverId: this.props.user, content: this.state.input })
+			.then(response => {
+				this.setState({input: ""})
+				console.log(response);
+			}).catch(error => {
+				console.log(error);
+			})
+	};
 
 	render() { 
 		return (
 			<div className="chatScreen">
 			{/* <p className = "chatScreen__timeStamp"> {`You matched with Kunika on 28 / 4 / 2019 `}</p> */}
-			{this.state.messages.map((arrMess) => 
-			arrMess.username == this.props.bio.username ? (
-				<div className="chatScreen__messages">
+			{this.state.messages.reverse().map((arrMess, index) => 
+			arrMess.receiverUsername != this.props.bio.username ? (
+				<div className="chatScreen__messages" key={index}>
 				<Avatar
-				src={arrMess.image} 
-				alt={arrMess.name}
+				src={arrMess.senderPhotoUrl} 
+				alt={arrMess.senderUsername}
+				key={index}
 				/>
-				<p className="chatScreen__text">{arrMess.message}</p>
+				<p className="chatScreen__text" key={index}>{arrMess.content}</p>
 			</div>
 			):(
-				<div className = "chatScreen__messages">
-					<p className="chatScreen__textUser">{arrMess.message}</p>
+				<div className = "chatScreen__messages" key={index}>
+					<p className="chatScreen__textUser">{arrMess.content}</p>
 				</div>
 			)
 			)}
 			
-			<form className = "chatScreen__input">
+			<form className = "chatScreen__input" onSubmit={this.submitHandler} action="#">
 				<input className="chatScreen__inputField" type="text" placeholder="Write a message..." value={this.state.input} onChange={this.changeHandler} />
-				<button type="submit" onClick={this.submitHandler} className="chatScreen__inputButton">SEND</button>
+				<button type="submit" className="chatScreen__inputButton">SEND</button>
 			</form>
 		</div>);
 	}
--- ClientApp/src/components/Chats/Chats.js
+++ ClientApp/src/components/Chats/Chats.js
@@ -6,7 +6,7 @@ import "./Chats.css";
 class Chats extends Component {
 	constructor(props) {
     super(props);
-    this.state = { userdata: []};
+    this.state = { userdata: [], preusername: "", sortedSendMessages: {}, sortedReceivedMessages: {}};
   }
 
 	componentDidMount() {
@@ -16,30 +16,84 @@ class Chats extends Component {
 			.then(response => {
 				this.setState({ userdata: response.data });
 				console.log(this.state.userdata);
+				this.sortingTheMessages();
 			}).catch(error => {
 				console.log(error);
 			})
 	};
+
+	sortingTheMessages = () => {
+		let previousUsername = "";
+		let previousSenderUsername = this.state.userdata[0].senderUsername;
+		this.state.userdata.reverse().forEach(user => {
+
+			//if the sender is logedIn user ==> messages send
+			if (user.senderUsername === this.props.bio.username) {
+				// console.log("test1")
+				if (user.receiverUsername !== previousUsername)
+					{
+					this.setState({
+						sortedSendMessages: {
+							receiverUsername: user.receiverUsername,
+							messages: [...this.state.sortedSendMessages.messages, user.content]
+						}
+					});
+					previousUsername = user.receiverUsername;
+					}//end of the inner if
+			}//end of outer if
+
+			if (user.receiverUsername === this.props.bio.username)
+			{
+				// console.log("test2")
+				console.log(user);
+				console.log(previousSenderUsername);
+				if (user.senderUsername === previousSenderUsername)
+				{
+					// console.log("test3")
+					this.setState({
+						sortedReceivedMessages: {
+							senderId: user.senderId,
+							senderUsername: user.senderUsername,
+							message: user.content
+						}
+					});
+					previousSenderUsername = user.senderUsername;
+						}//end of inner if
+			}//end of outer if
+			
+		});
+		console.log(this.state.sortedReceivedMessages);
+		console.log(this.state.sortedSendMessages);
+	}
 	
 	render() {
-		return (
-			<div>{
-				this.state.userdata.map((user, index) =>
-					<Chat
-						chatUser={(receiverId, username) => this.props.chatUser(receiverId, username)}
-						key={index}
-						username={user.senderUsername}
-						message={user.content}
-						id={user.senderId}
-						timeStamp={user.messageSent}
-						profilePic={(user.senderPhotoUrl == null) ?
-							"https://images.pexels.com/photos/1368382/pexels-photo-1368382.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-							: user.senderPhotoUrl}
 
-			/>
-				)}
-		</div>
-		);
+		
+		// const chatsFromState = this.state.sortedReceivedMessages.map((user, index) =>
+				
+		// 				<Chat
+		// 					chatUser={(receiverId, username) => this.props.chatUser(receiverId, username)}
+		// 					key={index}
+		// 					username={user.senderUsername}
+		// 					message={user.content}
+		// 					id={user.senderId}
+		// 					timeStamp={user.messageSent}
+		// 					profilePic={(user.senderPhotoUrl == null) ?
+		// 						"https://images.pexels.com/photos/1368382/pexels-photo-1368382.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+		// 						: user.senderPhotoUrl}
+		// 				/>
+		// 		)
+
+		return (
+			<div>
+				<Chat
+							chatUser={(receiverId, username) => this.props.chatUser(receiverId, username)}
+							id={this.state.sortedReceivedMessages.senderId}
+							username={this.state.sortedReceivedMessages.senderUsername}
+							message={this.state.sortedReceivedMessages.message}
+						/>
+			</div>
+		)
 	}
 }
 
